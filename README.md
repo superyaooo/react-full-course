eeb5589495db81837745bc54f6a5d18ae4269a70
superyaooo <superyaooo@gmail.com>
2024-06-29 11:40:47 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# React JS Full Course 2024
+## [Youtube link](https://youtu.be/lAFbKzO-fss?si=DcKCG1lvghQuZUMq) 
+
+
+
+### React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,19 @@
+export function UserProfile() {
+    return (
+        <div id="user-profile">
+            <p>Username: Bob</p>
+            <div>
+                <span>Email:</span>
+                <span>test@test.com</span>
+            </div>
+            <section>
+                <spa>Fav foods:</spa>
+                <br/>
+                <ul>
+                    <li>Sushi</li>
+                    <li>Ramen</li>
+                </ul>
+            </section>
+        </div>
+    );
+}
