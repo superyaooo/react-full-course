0bcef6c790f787c5dc5c9c1b29504039381e7700
superyaooo <superyaooo@gmail.com>
2024-09-07 12:34:31 -0500

    add elements to state array

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
-import { RegisterForm } from "./components/RegisterForm";
+import { useState } from "react";
+import { UserDetails } from "./components/UserDetails";
 
 export default function App() {
-    window.addEventListener("resize", (e) => {
-        console.log(window.innerHeight, window.innerWidth);
-    });
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [counter, setCounter] = useState(3);
+
+    const [users, setUsers] = useState([
+        {
+            id: 1,
+            username: "fjka",
+            email: "kjalj@fa.com"
+        },
+        {
+            id: 2,
+            username: "fadgag",
+            email: "fafag@fa.com"
+        },
+    ]);
 
     return (
-        <div>
-            <RegisterForm />
-        </div>
-    );
+		<div>
+			<form onSubmit={e => {
+				e.preventDefault();
+				const newUser = {
+					id: counter,
+					username,
+					email
+				}
+				setCounter((currCounter) => currCounter + 1);
+				setUsers((currUsersState) => [...currUsersState, newUser]);
+			}}>
+				<div>
+					<label htmlFor='username'>Username</label>
+					<input name='username' id='username' value={username} onChange={(e) => {
+						setUsername(e.target.value);
+					}} />
+				</div>
+				<div>
+					<label htmlFor='email'>Email</label>
+					<input name='email' id='email' value={email} onChange={(e) => {
+						setEmail(e.target.value);
+					}} />
+				</div>
+				<button>Add User</button>
+			</form>
+			<br/>
+			{users.map((user) => (
+				<UserDetails key={user.id} user={user} setUsers={setUsers} />
+			))}
+		</div>
+	);
 }
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,16 +1,69 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
-export function UserDetails({ user }) {
+export function UserDetails({ user, setUsers }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [username, setUsername] = useState(user.username);
+    const [useremail, setUserEmail] = useState(user.email);
+
     return (
         <div>
-            <b>ID: </b>
-            <span>{user.id}</span>
-            <br/>
-            <b>Username: </b>
-            <span>{user.username}</span>
-            <br/>
-            <b>Email: </b>
-            <span>{user.email}</span>
+            <div>
+                <button onClick={() => {
+                    setIsEditing((currentState) => !currentState);
+                }}>
+                    Edit
+                </button>
+                <button onClick={() => {
+                    setUsers((currUsersState) => currUsersState.filter(
+                        (currUser) => currUser.id !== user.id
+                    ));
+                }}>
+                    Delete
+                </button>
+                {isEditing && (
+                    <button 
+                        onClick={() => {
+                            setUsers((currUsersState) => 
+                                currUsersState.map((currUser) => 
+                                    currUser.id === user.id 
+                                        ? {...currUser, username: username, email: useremail}
+                                        : currUser
+                                    )
+                                );
+                            setIsEditing(false);
+                        }}
+                    >
+                    Save
+                </button>
+                )}
+            </div>
+            <div>
+                <b>ID: </b>
+                <span>{user.id}</span>
+                <br/>
+                <b>Username: </b>
+                    {isEditing ? (
+                        <input name="username" id="username" value={username} 
+                            onChange={(e) => {
+                                setUsername(e.target.value);
+                            }}
+                        />
+                    ) : (
+                        <span>{user.username}</span>
+                    )}
+                <br/>
+                <b>Email: </b>
+                    {isEditing ? (
+                        <input name="email" id="email" value={useremail} 
+                            onChange={(e) => {
+                                setUserEmail(e.target.value);
+                            }}
+                        />
+                    ) : (
+                        <span>{user.email}</span>
+                    )}
+            </div>
         </div>
     );
 }
@@ -20,5 +73,6 @@ UserDetails.propTypes = {
         id: PropTypes.number.isRequired,
         username: PropTypes.string.isRequired,
         email: PropTypes.string.isRequired
-    })
+    }).isRequired,
+    setUsers: PropTypes.func.isRequired
 }
