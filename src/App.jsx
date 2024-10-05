d2e960832a0fab451c98d88786c7db10d33812b3
superyaooo <superyaooo@gmail.com>
2024-10-05 11:09:48 -0500

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,18 @@
-import { useState } from "react";
-import { UserDetails } from "./components/UserDetails";
+import { useState, useEffect } from "react";
 
 export default function App() {
-	const [username, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [counter, setCounter] = useState(3);
+	const [counter, setCounter] = useState(0);
+	const [sync, setSync] = useState(false);
 
-    const [users, setUsers] = useState([
-        {
-            id: 1,
-            username: "fjka",
-            email: "kjalj@fa.com"
-        },
-        {
-            id: 2,
-            username: "fadgag",
-            email: "fafag@fa.com"
-        },
-    ]);
+	useEffect(() => {
+		document.title = "React Tutorial ";
+	}, [sync]);
 
     return (
 		<div>
-			<form onSubmit={e => {
-				e.preventDefault();
-				const newUser = {
-					id: counter,
-					username,
-					email
-				}
-				setCounter((currCounter) => currCounter + 1);
-				setUsers((currUsersState) => [...currUsersState, newUser]);
-			}}>
-				<div>
-					<label htmlFor='username'>Username</label>
-					<input name='username' id='username' value={username} onChange={(e) => {
-						setUsername(e.target.value);
-					}} />
-				</div>
-				<div>
-					<label htmlFor='email'>Email</label>
-					<input name='email' id='email' value={email} onChange={(e) => {
-						setEmail(e.target.value);
-					}} />
-				</div>
-				<button>Add User</button>
-			</form>
-			<br/>
-			{users.map((user) => (
-				<UserDetails key={user.id} user={user} setUsers={setUsers} />
-			))}
+			<div>You clicked the button {counter} times.</div>
+			<button onClick={() => setCounter((counter) => counter + 1)}>Click Me</button>
+			<button onClick={() => setSync((current) => !current)}>Sync</button>
 		</div>
 	);
 }
